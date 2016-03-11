7ec5221358b2453912776be3490409ba412e9deb
Solomin Andrey <solomin.andrey@gmail.com>
2016-03-11 21:05:50 +0300

    Initial commit

---
+++ app/src/main/java/ru/solandme/stopwatch/StopwatchActivity.java
@@ -0,0 +1,57 @@
+package ru.solandme.stopwatch;
+
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class StopwatchActivity extends AppCompatActivity {
+    TextView timerView;
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        timerView = (TextView) findViewById(R.id.timer);
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = hours/60;
+                int secs = seconds%60;
+                String timer = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timerView.setText(timer);
+
+                if (running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+
+            }
+        });
+    }
+}
