c4ee9cb8f1beb662c897e0b7faca2426fac16940
Solomin Andrey <solomin.andrey@gmail.com>
2016-03-12 15:51:34 +0300

    При повороте сохраняет состояние и восстанавливает

--- app/src/main/java/ru/solandme/stopwatch/StopwatchActivity.java
+++ app/src/main/java/ru/solandme/stopwatch/StopwatchActivity.java
@@ -11,13 +11,44 @@ public class StopwatchActivity extends AppCompatActivity {
     TextView timerView;
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
         timerView = (TextView) findViewById(R.id.timer);
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
         runTimer();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("running", running);
+        outState.putBoolean("wasRunning", wasRunning);
+
     }
 
     public void onClickStart(View view) {
@@ -40,13 +71,13 @@ public class StopwatchActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours = seconds/3600;
-                int minutes = hours/60;
-                int secs = seconds%60;
+                int hours = seconds / 3600;
+                int minutes = hours / 60;
+                int secs = seconds % 60;
                 String timer = String.format("%d:%02d:%02d", hours, minutes, secs);
                 timerView.setText(timer);
 
-                if (running){
+                if (running) {
                     seconds++;
                 }
                 handler.postDelayed(this, 1000);
