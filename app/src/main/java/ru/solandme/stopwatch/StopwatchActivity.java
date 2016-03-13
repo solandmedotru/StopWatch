dac0068be74279feadc4ece4c7f34045437b9c10
Solomin Andrey <solomin.andrey@gmail.com>
2016-03-13 10:49:17 +0300

    Переделал фидбэки onPause and onResume

--- app/src/main/java/ru/solandme/stopwatch/StopwatchActivity.java
+++ app/src/main/java/ru/solandme/stopwatch/StopwatchActivity.java
@@ -28,15 +28,15 @@ public class StopwatchActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         wasRunning = running;
         running = false;
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         if (wasRunning) {
             running = true;
         }
